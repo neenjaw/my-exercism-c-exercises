b6e3297d0a425b939cbfe8666a24058ed2fcf670
Tim Austin <tim@neenjaw.com>
2020-01-14 01:23:13 -0600

    armstrong

--- armstrong-numbers/src/armstrong_numbers.c
+++ armstrong-numbers/src/armstrong_numbers.c
@@ -1,1 +1,35 @@
 #include "armstrong_numbers.h"
+
+int is_armstrong_number(int candidate)
+{
+  int digitCount = 0;
+  int digits[7];
+
+  int num = candidate;
+  do
+  {
+    digits[digitCount] = num - (num / 10 * 10);
+    digitCount++;
+    num /= 10;
+  } while (num != 0);
+
+  int sum = 0;
+  for (int i = 0; i < digitCount; i++)
+  {
+    sum += power(digits[i], digitCount);
+  }
+
+  return sum == candidate;
+}
+
+int power(int base, int exp)
+{
+  int result = 1;
+  while (exp != 0)
+  {
+    result *= base;
+    --exp;
+  }
+
+  return result;
+}
--- armstrong-numbers/src/armstrong_numbers.h
+++ armstrong-numbers/src/armstrong_numbers.h
@@ -1,6 +1,8 @@
 #ifndef ARMSTRONG_NUMBERS
 #define ARMSTRONG_NUMBERS
 
+int power(int base, int exp);
+
 int is_armstrong_number(int candidate);
 
 #endif
